95f03d2f50bbba5967ba203beb3de7a71de6a3d9
mohinid <mohinidahiya4@gmail.com>
2023-03-16 20:34:31 +0530

    add token authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  acts_as_token_authentication_handler_for User, only: :create
+  protect_from_forgery with: :null_session
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "home#index"
 
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do 
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+      end
+    end
+  end
 end
