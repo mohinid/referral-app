3f3a92f6c4ceae5a93ceadfd57c49832605bf59d
mohinid <mohinidahiya4@gmail.com>
2023-04-05 11:15:07 +0530

    update rspecs for referrals_controller

--- spec/controller/referral_controller_spec.rb
+++ spec/controller/referral_controller_spec.rb
@@ -1,51 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe ReferralsController, type: :controller do
-  describe 'POST #create' do
+  describe "POST #create" do
     let(:user) { create(:user) }
+    let(:valid_params) { { email: "test@example.com" } }
 
-    context 'when referral email is unique' do
+    context "when email is sent successfully" do
       before do
-        sign_in user
         allow(ReferralMailer).to receive_message_chain(:with, :referral_email, :deliver_now)
-        post :create, params: { email: Faker::Internet.email }
+        sign_in user
+        post :create, params: valid_params
       end
 
-      it 'sends a referral email' do
-        expect(ReferralMailer).to have_received(:with)
-      end
-
-      it 'creates a new referral' do
+      it "creates a new referral" do
         expect(user.referrals.count).to eq(1)
+        expect(user.referrals.first.email).to eq(valid_params[:email])
       end
 
-      it 'redirects to root path' do
+      it "sets the flash notice message" do
+        expect(flash[:notice]).to eq("Email to '#{valid_params[:email]}' sent and saved for reference!")
+      end
+
+      it "redirects to root path" do
         expect(response).to redirect_to(root_path)
-      end
-
-      it 'sets the flash notice' do
-        expect(flash[:notice]).to be_present
       end
     end
 
-    context 'when referral email is a duplicate' do
+    context "when email sending fails" do
       before do
+        allow(ReferralMailer).to receive_message_chain(:with, :referral_email).and_raise(StandardError.new("email sending failed"))
         sign_in user
-        create(:user, email: 'test@example.com')
-        create(:referral, email: 'test@example.com')
-        post :create, params: { email: 'test@example.com' }
+        post :create, params: valid_params
       end
 
-      it 'does not create a new referral' do
+      it "does not create a new referral" do
         expect(user.referrals.count).to eq(0)
       end
 
-      it 'redirects to root path' do
-        expect(response).to redirect_to(root_path)
+      it "sets the flash alert message" do
+        expect(flash[:alert]).to eq("Failed to send referral email, please try again later.")
       end
 
-      it 'sets the flash notice' do
-        expect(flash[:alert]).to be_present
+      it "redirects to root path" do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "when email is a duplicate" do
+      before do
+        create(:referral, email: valid_params[:email])
+        sign_in user
+        post :create, params: valid_params
+      end
+
+      it "does not create a new referral" do
+        expect(user.referrals.count).to eq(0)
+      end
+
+      it "sets the flash alert message" do
+        expect(flash[:alert]).to eq("Email '#{valid_params[:email]}' already exists as a referred user or subscriber. Please use another email.")
+      end
+
+      it "redirects to root path" do
+        expect(response).to redirect_to(root_path)
       end
     end
   end
